897c02e46b89516ebce1369e62baeca7272d64d0
Maria Isabel Restrepo <isabelrestre@gmail.com>
2018-05-09 11:26:59 -0400

    Add unregistered - remove LowRankModels

--- src/BCBI_base.jl
+++ src/BCBI_base.jl
@@ -3,48 +3,52 @@ module BCBI_base
 
 export add_and_precompile
 
-const base_pkgs = [ "MySQL",
-                    "StatsBase",
-                    "DataFrames",
-                    "CSV",
-                    "Query",
-                    "Clustering",
-                    "DecisionTree",
-                    "GLM",
-                    "GLMNet",
-                    "HypothesisTests",
-                    "LowRankModels",
-                    "Lasso",
-                    "MixedModels",
-                    "JuliaDB",
-                    "HTTP",
-                    "BioServices",
-                    "BioMedQuery",
-                    "JLD",
-                    "JLD2",
-                    "EzXML",
-                    "LightXML",
-                    "RCall",
-                    "PyCall",
-                    "ScikitLearn",
-                    "Gadfly",
-                    "PyPlot",
-                    "Seaborn",
-                    "Pandas",
-                    "Revise"]
+const registered_pkgs = [   "MySQL",
+                            "StatsBase",
+                            "DataFrames",
+                            "CSV",
+                            "Query",
+                            "Clustering",
+                            "DecisionTree",
+                            "GLM",
+                            "GLMNet",
+                            "HypothesisTests",
+                            # "LowRankModels", downgrades deps
+                            "Lasso",
+                            "MixedModels",
+                            "JuliaDB",
+                            "HTTP",
+                            "BioServices",
+                            "BioMedQuery",
+                            "JLD",
+                            "JLD2",
+                            "EzXML",
+                            "LightXML",
+                            "RCall",
+                            "PyCall",
+                            "ScikitLearn",
+                            "Gadfly",
+                            "PyPlot",
+                            "Seaborn",
+                            "Pandas",
+                            "Revise"]
+
+const unregistered_pkgs =[ "https://github.com/bcbi/ClassImbalance.jl.git",
+                           "https://github.com/bcbi/ARules.jl"]
 
 
-function add_and_precompile()
+function install_regitered()
 
     failed_pkgs = Vector{String}()
 
-    for pkg in base_pkgs
+    for pkg in registered_pkgs
         println("--------------------------------")
-        println("Adding package: ", pkg)
+        println("Package: ", pkg)
         println("--------------------------------")
 
 
         try
+            println("* Adding")
             Pkg.add(pkg)
         catch
             warn("Pkg.add failed")
@@ -52,10 +56,8 @@ function add_and_precompile()
             continue
         end
 
-        println("--------------------------------")
-        println("Precompiling package: ", pkg)
-        println("--------------------------------")
         try
+            println("* Using")
             pkgsym = Symbol(pkg)
             eval(:(using $pkgsym))
         catch
@@ -70,7 +72,43 @@ function add_and_precompile()
     println("Failed packages: ")
     map(x->println(x), failed_pkgs)
     println("--------------------------------")
+end
 
+function install_unregitered()
+
+    failed_pkgs = Vector{String}()
+
+    for pkg in unregistered_pkgs
+        println("--------------------------------")
+        println("Package: ", pkg)
+        println("--------------------------------")
+
+
+        try
+            println("* Clonning")
+            Pkg.clone(pkg)
+        catch
+            warn("Pkg.clone failed")
+            push!(failed_pkgs, pkg)
+            continue
+        end
+
+        try
+            println("* Using")
+            pkgsym = Symbol(pkg)
+            eval(:(using $pkgsym))
+        catch
+            warn("using pkg failed")
+            push!(failed_pkgs, pkg)
+            Pkg.rm(pkg)
+        end
+
+    end
+
+    println("--------------------------------")
+    println("Failed packages: ")
+    map(x->println(x), failed_pkgs)
+    println("--------------------------------")
 end
 
 
