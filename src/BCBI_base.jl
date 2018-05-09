852262eb60eb4ec9dfe3ba8a580730609ba336f9
Isabel Restrepo (pencil) <isabelrestre@gmail.com>
2018-05-09 17:14:45 -0400

    add a function for packages that need to check out a particular branch

--- src/BCBI_base.jl
+++ src/BCBI_base.jl
@@ -2,7 +2,8 @@ module BCBI_base
 
 
 export install_registered,
-       install_unregistered
+       install_unregistered,
+       install_checkedout
 
 const registered_pkgs = [   "MySQL",
                             "StatsBase",
@@ -27,16 +28,18 @@ const registered_pkgs = [   "MySQL",
                             "LightXML",
                             "RCall",
                             "PyCall",
-                            # "ScikitLearn", downgrades deps
-                            # "Gadfly",
-                            # "PyPlot",
-                            # "Seaborn",
-                            # "Pandas",
-                            "Revise"]
+                            # "Gadfly", downgrades deps
+                            "PyPlot", #get WARNING: No working GUI backend found for matplotlib
+                            "Seaborn",
+                            "Pandas",
+                            "Revise",
+                            "IJulia"]
 
 const unregistered_pkgs =[ "https://github.com/bcbi/ClassImbalance.jl.git",
                            "https://github.com/bcbi/ARules.jl"]
 
+
+const dirty_pkgs = Dict("ScikitLearn" => "master")
 
 function install_registered()
 
@@ -106,6 +109,31 @@ function install_unregistered()
             Pkg.rm(pkg)
         end
 
+    end
+
+    println("--------------------------------")
+    println("Failed packages: ")
+    map(x->println(x), failed_pkgs)
+    println("--------------------------------")
+end
+
+function install_checkedout()
+    failed_pkgs = Vector{String}()
+
+    for (pkg, branch) in dirty_pkgs
+        println("--------------------------------")
+        println("Package: ", pkg)
+        println("--------------------------------")
+        Pkg.clone(pkg, branch)
+        try
+            println("* Using")
+            pkgsym = Symbol(pkg)
+            eval(:(using $pkgsym))
+        catch
+            warn("using pkg failed")
+            push!(failed_pkgs, pkg)
+            Pkg.rm(pkg)
+        end
     end
 
     println("--------------------------------")
