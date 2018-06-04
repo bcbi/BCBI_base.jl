077f1fff829c0f3c40d1fa7b3d6064b87fa509e4
Maria Isabel Restrepo <isabelrestre@gmail.com>
2018-06-04 15:58:12 -0400

    double quotes

--- postpocess_julia_deps.sh
+++ postpocess_julia_deps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 #replaces all instances of /opt/conda/ for /opt/browncis/conda/ so deps are found in the workstations
-find $JULIA_PKGDIR/v0.6 -name "deps.jl" -type f -exec sed -i 's+/opt/conda/envs/"$CONDA_DEFAULT_ENV"+/opt/browncis/conda/envs/"$CONDA_DEFAULT_ENV"+g' {} +
+find $JULIA_PKGDIR/v0.6 -name "deps.jl" -type f -exec sed -i "s+/opt/conda/envs/$CONDA_DEFAULT_ENV+/opt/browncis/conda/envs/$CONDA_DEFAULT_ENV+g" {} +
